576a7453e73fe327df0d169d623a15fae6bca174
[surferjreb] <[surferjreb@live.com]>
2022-01-09 13:32:58 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+
+# Django_Social
+### *By: James R. E. Brown*
+
+> ## Description:  
+>  A simple social app created using a tutorial to refresh python web app development.
+>
+>  
+  
+> ## Dependecies:
+>  django: 3.2.5
+>
+>  
+  
+> ## Issues:  
+>
+>
+>  
+  
+> ## Fixes:  
+  
+> ## Versions:  
+>  1.0
+>
+>  
+
+
+
+
---
+++ dwitter/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth.models import User, Group
+
+
+# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Only display the "username" field
+    fields = ["username"]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
