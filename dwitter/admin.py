3e4f3e08a74085e021be22b7daf06ddbe12e30ed
[surferjreb] <[surferjreb@live.com]>
2022-01-09 22:14:17 -0500

    extended User and adjusted admin

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
+from .models import Profile
 
 
 # Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
 class UserAdmin(admin.ModelAdmin):
     model = User
     # Only display the "username" field
     fields = ["username"]
+    inlines = [ProfileInline]
 
 
 admin.site.unregister(User)
