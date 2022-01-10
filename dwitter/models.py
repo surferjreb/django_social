a77fa3f495386e0d76fd07bca26bf3b0347746df
[surferjreb] <[surferjreb@live.com]>
2022-01-09 22:33:07 -0500

    refactored models.py

--- dwitter/models.py
+++ dwitter/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -15,11 +16,10 @@ class Profile(models.Model):
         return self.user.username
 
 
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
         user_profile.save()
-
-
-# Create a Profile for each new user.
-post_save.connect(create_profile, sender=User)
+        user_profile.follows.add(instance.profile)
+        user_profile.save()
